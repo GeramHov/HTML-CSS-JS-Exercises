1798b04c73d142d265101bd9caa25cf543daf131
GeramHov <geram.hov@gmail.com>
2023-01-09 00:12:18 +0100

    pending

---
+++ README.txt
@@ -0,0 +1,46 @@
+::● Créez 9 cases de taille identique.
+::● Au clic sur une case, la couleur de la case change.
+::● Si je clic sur la case à nouveaux, elle change encore de couleur.
+::● Le nombre de couleurs différentes doit être de minimum 5.
+::● Le clic d&#39;une case ne doit pas changer la couleur de ses voisines.
+
+2
+
+��️ Modale Caché
+● :: Créez une case de 200 px par 200 px qui contiennent un texte et
+un background color blanc.
+● ::Faire en sorte que cette case ne soit pas visible.
+● :: Créez un bouton quelque part dans la page.
+● :: Au clic du bouton, la case de texte apparaît.
+● :: Le background de la page devient gris foncé mais la couleur de la
+case reste blanche.
+● :: Si je clic en dehors de la case, elle disparaît et le background de la
+page redevient blanc.
+��️ Combinaison gagnante
+● :: Créez un paragraphe avec une consigne de combinaison de 2
+touches à appuyer.
+● Lorsque la combinaison est pressée, une phrase apparaît dans un
+autre paragraphe.
+● Bonus : si j&#39;appuie sur une troisième touche, le texte change de
+couleur.
+��️ Hover size
+
+3
+
+● ::Créez 4 cases de 200 px par 200 px.
+● ::Chaque cases contient un texte, des bordures noires de 2 px et un
+blur de 60 %.
+● ::Au hover de la souris le blur disparaît et les bordures deviennent
+rouges.
+●:: Si je maintiens le clic de la souris sur la case, les dimensions de la
+case deviennent 400px de large pour 200 px de haut.
+● ::Quand le clic est relâché la case reprend sa dimension d&#39;origine.
+
+�� Bonus : �� Carrousel
+● ::Créez un bouton
+● ::Au clic du bouton une image apparaît
+● Quand je clic à droite de l&#39;image, une autre image apparaît
+○ bonus : faire un effet de transition
+● Si je clic à droite de l&#39;image une troisième apparaît, si je clic à
+gauche la précédente apparaît
+● ⚠️ interdiction d&#39;utiliser les carrousels des frameworks css !
---
+++ main.js
@@ -0,0 +1,177 @@
+///////////// EX 1.2 ///////////////
+
+// let ofColor = document.getElementById('casefive');
+
+// ofColor.addEventListener('click', function(event){
+//     ofColor.style.backgroundColor = 'green'
+//     event.stopPropagation()
+// })
+
+// document.addEventListener('click', function(){
+//     ofColor.style.backgroundColor = 'bisque'
+// })
+
+//////////////////////////////////
+
+///////////// EX 1.3 ///////////////
+
+// let myCase = document.getElementById('casefive');
+
+// myCase.addEventListener('click', function(){
+//     myCase.style.backgroundColor = 'red'
+//     if (myCase.style.backgroundColor === 'red') {
+//         myCase.addEventListener('click', function(){
+//             myCase.style.backgroundColor = 'green'
+//         })
+//     }
+// })
+//////////////////////////////////
+
+///////////// EX 1.4 ///////////////
+
+let myCase = document.querySelectorAll('.ex1')
+
+const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+let colorIndex = 0;
+
+for (let aCase of myCase) {
+aCase.addEventListener('click', function(event) {
+  aCase.style.backgroundColor = colors[colorIndex];
+  colorIndex = (colorIndex + 1) % colors.length;
+  event.stopPropagation()
+  document.addEventListener('click', function(){
+    aCase.style.backgroundColor = 'bisque'
+  })
+})};
+
+
+//////////////////////////////////
+
+///////////// EX 1.5 ///////////////
+
+// let someCase = document.getElementById('casefive');
+
+// const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+// let colorIndex = 0;
+
+// someCase.addEventListener('click', function(event){
+//    someCase.style.backgroundColor = colors[colorIndex];
+//    colorIndex = (colorIndex + 1) % colors.length;
+//    event.stopPropagation()
+// })
+//   document.addEventListener('click', function(){
+//     someCase.style.backgroundColor = 'bisque'
+//   })
+
+//////////////////////////////////
+
+///////////// EX 2.3 /////////////
+
+let textCase = document.querySelector('.newcase')
+
+textCase.style.visibility = 'hidden'
+
+//////////////////////////////////
+
+///////////// EX 2.5 2.6 /////////////
+
+let textBtn = document.getElementById('btn')
+
+let page = document.querySelector('body')
+
+textBtn.addEventListener('click', function(event){
+    textCase.style.visibility = 'visible';
+    page.style.backgroundColor = 'grey'
+    event.stopPropagation()
+})
+
+document.addEventListener('click', function(event){
+  if (!event.target.matches('#paragraph')){
+  textCase.style.visibility = 'hidden'
+  page.style.backgroundColor = 'white'
+}
+})
+//////////////////////////////////
+
+///////////// Combinaison gagnante /////////////
+
+let consigne = document.getElementById('consigne')
+
+consigne.style.filter = 'blur(50px)'
+
+consigne.addEventListener('mouseenter', function(){
+consigne.style.filter = 'blur(0)'
+});
+
+consigne.addEventListener('mouseleave', function(){
+  consigne.style.filter = 'blur(50px)'
+  })
+
+let hiddenString = document.getElementById('hiddenstring')
+
+hiddenString.style.visibility = 'hidden'
+
+document.addEventListener('keydown', function(event){
+  if (event.key === ('e','r')) {
+hiddenString.style.visibility = 'visible'
+  }
+})
+
+document.addEventListener('keyup', function(event){
+  if (event.key === ('e','r')) {
+hiddenString.style.visibility = 'hidden'
+  } 
+})
+
+////////////////////////////////////////////////
+
+////////////////// EXERCISE 3 //////////////////
+
+let zoomExs = document.querySelectorAll('#cas')
+
+for (let zoomEx of zoomExs) {
+    zoomEx.addEventListener('mousedown', function(){
+    zoomEx.style.width = '400px'
+    })
+    
+    zoomEx.addEventListener('mouseup', function(){
+    zoomEx.style.width = '200px'
+    })
+}
+
+////////////////////////////////////////////////
+
+////////////////// BONUS ///////////////////////
+
+let pic = document.querySelector('.masis')
+let button = document.querySelector('#btn-photo')
+let rightclick = document.querySelector('.rightclick')
+
+pic.style.visibility = 'hidden'
+
+button.addEventListener('click', function(){
+  pic.style.visibility = 'visible'
+})
+
+
+
+
+
+
+
+////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
